9ef9c991f80083fec841d902ad40c94f3a711291
GBhack <gbmail01@gmail.com>
2016-10-11 19:12:21 +0200

    client to test us

--- client.py
+++ client.py
@@ -6,20 +6,25 @@
 #!/usr/bin/python3.5
 #-*- coding: utf-8 -*-
 
+import time
 import robotBasics as RB
 import atexit
 
 SOCKETS = RB.sockets
 
-TCP = SOCKETS.tcp.Client.Client(12345)
+TCP = SOCKETS.tcp.Client.Client(1300)
 
 TCP.set_sending_datagram(['BOOL'])
 TCP.set_receiving_datagram(['FLOAT'])
 
 TCP.set_up_connexion()
 
-TCP.send_data([True])
+while 1:
 
-print(TCP.receive_data()[0])
+    TCP.send_data([True])
+
+    print(TCP.receive_data()[0])
+
+    time.sleep(2)
 
 atexit.register(TCP.close)
