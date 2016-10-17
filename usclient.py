3ec174f7211577c6fd60eb8a900f537efab241d7
GBhack <gbmail01@gmail.com>
2016-10-17 15:26:53 +0200

    test client for us module

---
+++ usclient.py
@@ -0,0 +1,29 @@
+"""
+    client.py
+"""
+
+
+#!/usr/bin/python3.5
+#-*- coding: utf-8 -*-
+
+import time
+import robotBasics as RB
+import atexit
+
+SOCKETS = RB.sockets
+
+TCP = SOCKETS.tcp.Client.Client(1300)
+
+atexit.register(TCP.close)
+
+TCP.set_sending_datagram(['BOOL'])
+TCP.set_receiving_datagram(['FLOAT'])
+
+if TCP.set_up_connection(600):
+
+    while 1:
+
+        TCP.send_data([True])
+        time.sleep(0.5)
+        print(TCP.receive_data())
+        time.sleep(2)
