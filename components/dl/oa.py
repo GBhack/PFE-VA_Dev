c37bf8eb7e52b1fa350b9c3895b9611cf242ef7c
GBhack <gbmail01@gmail.com>
2016-10-21 10:16:49 +0200

    Updates

--- components/dl/oa.py
+++ components/dl/oa.py
@@ -10,47 +10,48 @@
 
 #Standard imports :
 import time
+import atexit
 
 #Specific imports :
-import robotBasics as RB
-
-CONSTANTS = RB.constants
+from robotBasics import constants as CONSTANTS
+from robotBasics import sockets as SOCKETS
 
 UPDATE_RATE = 0.2
 MINIMAL_DISTANCE = 0.075
 
-SOCKETS = RB.sockets
+###########################################################################
+#                     CONNECTIONS SET UP AND SETTINGS :                   #
+###########################################################################
+
+#### CLIENTS CONNECTION :
 
 #Creating the Get Frontal Distance module's client
-GFD_CLIENT = SOCKETS.tcp.Client.Client(CONSTANTS.ports.ECL["gfd"])
+UC_CLIENT = SOCKETS.tcp.Client.Client(CONSTANTS.ports.ECL["uc"])
 
 #We'll send booleans (request)
-GFD_CLIENT.set_sending_datagram(['BOOL'])
+UC_CLIENT.set_sending_datagram(['BOOL'])
 #We'll receive floats (distance in meters)
-GFD_CLIENT.set_receiving_datagram(['FLOAT'])
+UC_CLIENT.set_receiving_datagram(['FLOAT'])
 
 #Opening the connexion
-GFD_CLIENT.set_up_connexion()
+UC_CLIENT.set_up_connexion()
 
 
 #Creating the Velocity/Steering regulator module's client object
-VSR_CLIENT = SOCKETS.tcp.Client.Client(CONSTANTS.ports.ECL["vsr"]["brake"])
+VE_CLIENT = SOCKETS.tcp.Client.Client(CONSTANTS.ports.DL["ve"])
+#Registering the close method to be executed at exit (clean deconnection)
+atexit.register(VE_CLIENT.close)
 
 #We'll send floats (distance in meters)
-VSR_CLIENT.set_sending_datagram(['BOOL'])
+VE_CLIENT.set_sending_datagram(['BOOL'])
 
 #Opening the connexion
-VSR_CLIENT.set_up_connexion()
-
-stopped = False
+VE_CLIENT.set_up_connexion()
 
 while True:
-    GFD_CLIENT.send_data([True])
-    distance = GFD_CLIENT.receive_data()[0]
-    if distance <= MINIMAL_DISTANCE:
-        VSR_CLIENT.send_data(True)
-        stopped = True
-    elif stopped:
-        VSR_CLIENT.send_data(False)
-        stopped = False
-    time.sleep(UPDATE_RATE
+    UC_CLIENT.send_data([True])
+    if UC_CLIENT.receive_data()[0] <= MINIMAL_DISTANCE:
+        VE_CLIENT.send_data([True])
+    else:
+        VE_CLIENT.send_data([False])
+    time.sleep(UPDATE_RATE)
---
+++ components/dl/ve.py
@@ -0,0 +1,104 @@
+"""
+    ve.py
+    Decision Level module : Velocity Executor
+    Executes the velocity plan while handeling emergency brake
+    if requested by the oa module.
+"""
+
+
+#!/usr/bin/python3.5
+#-*- coding: utf-8 -*-
+
+#Standard imports :
+import time
+import atexit
+
+#Specific imports :
+from robotBasics import constants as CONSTANTS
+from robotBasics import sockets as SOCKETS
+
+VELOCITY_STATE = {
+    "busy": False,
+    "oa_brake": False,
+    "velocity": 0
+}
+
+###########################################################################
+#                     Functions/Callbacks definition :                    #
+###########################################################################
+
+def oa_handling_cb(data, args):
+    if data[0]:
+        while  args["velocity_state"]["busy"]:
+            time.sleep(0.0001)
+        args["velocity_state"]["busy"] = True
+        args["velocity_state"]["oa_brake"] = True
+        args["velocity_state"]["desiredVelocity"]
+        args["velocity_client"].send_data([0])
+        _ = args["velocity_client"].receive_data()
+        args["velocity_state"]["busy"] = False
+    else:
+        args["oa_brake"] = False
+
+
+###########################################################################
+#                     CONNECTIONS SET UP AND SETTINGS :                   #
+###########################################################################
+
+#### CLIENTS CONNECTION :
+
+#
+VELOCITY_CLIENT = SOCKETS.tcp.Client.Client(CONSTANTS.ports.ECL["uc"])
+
+#We'll send booleans (request)
+VELOCITY_CLIENT.set_sending_datagram(['FLOAT'])
+#We'll receive floats (distance in meters)
+VELOCITY_CLIENT.set_receiving_datagram(['FLOAT'])
+
+#Opening the connexion
+VELOCITY_CLIENT.set_up_connexion()
+
+#### SERVER CONNECTION :
+
+#Creating the TCP instances
+OA_SERVER = SOCKETS.tcp.Server.Server(CONSTANTS.ports.DL["ve"]["oa"])
+#Registering the close method to be executed at exit (clean deconnection)
+atexit.register(OA_SERVER.close)
+
+#We'll receive booleans (status of the operation)
+OA_SERVER.set_receiving_datagram(['BOOL'])
+
+#Opening the connection
+OA_SERVER.set_up_connection(10)
+
+## Velocity Server :
+
+#Creating the TCP instance
+VELOCITY_SERVER = SOCKETS.tcp.Server.Server(CONSTANTS.ports.DL["ve"]["velocity"])
+#Registering the close method to be executed at exit (clean deconnection)
+atexit.register(VELOCITY_SERVER.close)
+
+#We'll receive and send small integers (velocity in percent of nominal velocity)
+VELOCITY_SERVER.set_receiving_datagram(['FLOAT'])
+VELOCITY_SERVER.set_sending_datagram(['BOOL'])
+
+#Opening the connection
+VELOCITY_SERVER.set_up_connection(600)
+
+OA_ARGUMENTS = {
+    "velocity_state": VELOCITY_STATE,
+    "velocity_client": VELOCITY_CLIENT
+}
+
+VELOCITY_ARGUMENTS = {
+    "velocity_state": VELOCITY_STATE
+}
+
+while alive:
+    if VELOCITY_STATE["oa_brake"]:
+        desiredVelocity = 0
+    else:
+        desiredVelocity = VELOCITY_STATE["desiredVelocity"]
+    if VELOCITY_STATE["actualVelocity"] != desiredVelocity
+    VELOCITY_CLIENT.send_data([desiredVelocity])
+    VELOCITY_STATE["actualVelocity"] = VELOCITY_CLIENT.receive_data()
