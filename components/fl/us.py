b2e443b3afa087e0db75732ec5348ef1db4ba794
GBhack <gbmail01@gmail.com>
2016-10-21 15:54:53 +0200

    US mod

--- components/fl/us.py
+++ components/fl/us.py
@@ -24,9 +24,10 @@ def measure_distance_cb(data, arg):
         Called when a tcp request is received
         Trigger an ultrasonic measure and sends back the echo time in seconds
     """
+
     GPIO.output(RB.constants.gpiodef.SONAR["trigger"], GPIO.HIGH)
     GPIO.output(RB.constants.gpiodef.SONAR["trigger"], GPIO.LOW)
-
+    print('Wating for echo')
     while not GPIO.input(RB.constants.gpiodef.SONAR["echo"]):
         startTime = time.time()
 
