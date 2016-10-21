27291420379c648b277500c2fd1436259a671d25
GBhack <gbmail01@gmail.com>
2016-10-21 16:11:55 +0200

    Back to the future

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#python3 components/fl/us.py &
+python3 components/fl/us.py &
 sleep 0.5
 python3 components/fl/mot.py &
 sleep 0.5
-#python3 components/ecl/uc.py &
+python3 components/ecl/uc.py &
 sleep 0.5
 python3 components/ecl/vsc.py &
 sleep 0.5
-#python3 components/dl/ve.py &
-#sleep 0.5
-#python3 components/dl/oa.py &
+python3 components/dl/ve.py &
+sleep 0.5
+python3 components/dl/oa.py &
