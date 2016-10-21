a92de662ee063c8b49c919738dab9870f2d0e584
maximeIpsa <maxime.monnot@ipsa.fr>
2016-10-21 11:10:32 +0200

    Added comments in ports

--- robotBasics/constants/ports.py
+++ robotBasics/constants/ports.py
@@ -7,40 +7,39 @@
 #-*- coding: utf-8 -*-
 
 #Functional Level module's ports
-FL = {
-    "qe":   1000,
-    "os":   1100,
-    "mot":   {
+FL = {                  # FUNCTIONNAL LEVEL
+    "qe":   1000,           # Quadratic Encoder
+    "os":   1100,           # Optical Sensors
+    "mot":   {              # Motors
         "left":   1200,
         "right":  1210
     },
-    "leds":   1300,
-    "pb":  1400,
-    "ip": 1500,
-    "us": 1600
+    "leds":   1300,         # LEDs
+    "pb":  1400,            # PushButton
+    "ip": 1500,             # Image Processing
+    "us": 1600              # Ultrasonic Sensor
 }
 
-ECL = {
-    "gsc":  2000,
-    "vsc":  {
-        "velocity":     2100,
-        "radius":       2110
+ECL = {                 # EXECUTION CONTROL LEVEL
+    "gsc":  2000,           # General Sensors Controller (QE and OS)
+    "vsc":  {               # Velocity/Speed Controller
+        "velocity": 2100,
+        "radius":   2110
     },
-    "lc": 2200,
-    "mc": 2300,
-    "ipc": 2400,
-    "uc": 2500
+    "lc": 2200,             # LED Controller
+    "mc": 2300,             # Mode Controller (Play/Pause/Reset)
+    "ipc": 2400,            # Image Processing Controller
+    "uc": 2500              # Ultrasonic Controller
 }
 
-DL = {
-    "pe":  3000,
-    "ve":  {
-        "oa": 3100,
-        "velocity":     3110,
-        "radius":       3120
-    },
-    "oa":   3200,
-    "pf":   3300,
-    "vp":   3400,
-    "pp":  3500
+DL = {                  # DECISION LEVEL
+    "pe":  3000,            # Path Executor
+    "ve":  {                # Velocity Executor
+        "oa": 3100,         # Obstacle Avoidance
+        "velocity": 3110,   
+        "radius":   3120
+    },          
+    "pf":   3300,           # Path Following
+    "vp":   3400,           # Velocity Planning
+    "pp":   3500            # Path Planning
 }
