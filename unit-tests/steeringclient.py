a8bdf98f602669e8d0a3929c44829fc99ddb7aa1
GBhack <gbmail01@gmail.com>
2016-11-30 18:11:35 +0100

    Unit test updated with new socket class

--- unit-tests/steeringclient.py
+++ unit-tests/steeringclient.py
@@ -9,25 +9,24 @@
 import time
 import robotBasics as RB
 import atexit
-from robotBasics.logger import logger as LOGGER
-
+from robotBasics.sockets.tcp.Client import Client as Client
+from robotBasics.constants.connectionSettings import VSC as VSC_CS
 SOCKETS = RB.sockets
 
-TCP = SOCKETS.tcp.Client.Client(2210, LOGGER)
+LOGGER = robotLogger("unit-test > steeringclient", '')
+
+TCP = Client(VSC_CS["steering"], LOGGER)
 
 atexit.register(TCP.close)
 
-TCP.set_sending_datagram(['SMALL_INT_SIGNED'])
-TCP.set_receiving_datagram(['SMALL_INT_SIGNED'])
-
-if TCP.set_up_connection(600):
+if TCP.connect():
     while 1:
 
         value = int(input("Steering ?"))
         if value <= 100 and value >= -100:
             try:
-                TCP.send_data([int(value)])
+                TCP.send([int(value)])
             except:
                 print('erreur lors de l\'envoi')
             time.sleep(0.1)
-            print(TCP.receive_data())
+            print(TCP.receive())
