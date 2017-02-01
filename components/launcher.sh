aeed90ea32a4831f42c7c6caf8d01fd3d92d522f
GBhack <gbmail01@gmail.com>
2017-02-01 16:21:06 +0100

    Modified launcher

--- components/launcher.sh
+++ components/launcher.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $DIR
+
 #Functionnal Level
-sudo python3 fl/os.py &
+sudo python3 $DIR/$DIR/fl/os.py &
 sleep 2
-python3 fl/us.py &
+python3 $DIR/fl/us.py &
 sleep 0.1
-#python3 fl/qe.py &
+#python3 $DIR/fl/qe.py &
 #sleep 0.1
-python3 fl/pb.py &
+python3 $DIR/fl/pb.py &
 sleep 0.1
-python3 fl/mot.py &
+python3 $DIR/fl/mot.py &
 sleep 0.1
-python3 fl/led.py &
+python3 $DIR/fl/led.py &
 #Psleep 0.1
 
 #Execution Control Level
-python3 ecl/usc.py &
+python3 $DIR/ecl/usc.py &
 sleep 0.1
-python3 ecl/vsc.py &
+python3 $DIR/ecl/vsc.py &
 sleep 0.1
-#python3 ecl/qec.py &
+#python3 $DIR/ecl/qec.py &
 #sleep 0.1
-python3 ecl/pbc.py &
+python3 $DIR/ecl/pbc.py &
 sleep 0.1
-python3 ecl/ledc.py &
+python3 $DIR/ecl/ledc.py &
 
 #Decision Level
 sleep 0.1
-python3 dl/ve.py &
+python3 $DIR/dl/ve.py &
 sleep 0.1
-python3 dl/oa.py &
+python3 $DIR/dl/oa.py &
 sleep 0.1
-python3 dl/pfe.py &
+python3 $DIR/dl/pfe.py &
 sleep 0.1
-python3 dl/vp.py &
-
-#Waiting for a key to be pressed before quitting
-read -rsp $'Press any key to quit...\n' -n1 key
-
-killall python3
-sleep 0.1
-python3 abort.py
+python3 $DIR/dl/vp.py &
