7bedbc97cd415d903f230ee1b79b822374db6dc7
GBhack <gbmail01@gmail.com>
2017-02-01 14:23:02 +0100

    Improved launcher

--- components/launcher.sh
+++ components/launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Functionnal Level
 sudo python3 fl/os.py &
 sleep 2
 python3 fl/us.py &
@@ -8,11 +9,12 @@ sleep 0.1
 #sleep 0.1
 python3 fl/pb.py &
 sleep 0.1
-
 python3 fl/mot.py &
 sleep 0.1
 python3 fl/led.py &
 #Psleep 0.1
+
+#Execution Control Level
 python3 ecl/usc.py &
 sleep 0.1
 python3 ecl/vsc.py &
@@ -23,6 +25,7 @@ python3 ecl/pbc.py &
 sleep 0.1
 python3 ecl/ledc.py &
 
+#Decision Level
 sleep 0.1
 python3 dl/ve.py &
 sleep 0.1
@@ -30,4 +33,9 @@ python3 dl/oa.py &
 sleep 0.1
 python3 dl/pfe.py &
 sleep 0.1
-python3 dl/vp.py
+python3 dl/vp.py &
+
+#Waiting for a key to be pressed before quitting
+read -rsp $'Press any key to quit...\n' -n1 key
+
+killall python3
